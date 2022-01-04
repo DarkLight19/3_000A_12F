41eadc41d82da5877d47926ed5c5f15483961fe2
Imre <19darklight@gmail.com>
2022-01-04 14:03:25 +0100

    elso ora vege

---
+++ 3000A.py
@@ -0,0 +1,28 @@
+
+lista = []
+
+with open("03_000.txt" , "r", encoding="utf8") as f:
+    for i in f:
+        lista.append(int(i))
+
+print(f"Hány elemű a sorozat? {len(lista)}")
+
+print("Van e sorozatban negatív szám ? ")
+
+"""
+negative = 0
+for item in lista:
+    if item < 0:
+        negative = 1
+        break
+
+if negative == 1:
+    print("van")
+else:
+    print("nincs")
+"""
+i = 0
+while(i< len(lista) and not lista[i] < 0):
+    i+=1
+
+print("van" if i<len(lista) else "nincs")
